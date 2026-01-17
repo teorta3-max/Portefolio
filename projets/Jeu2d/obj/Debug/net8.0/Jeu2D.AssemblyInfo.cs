b26eeb8a66b45c028be3bc35f5d04d6f123de3b8
Flo <teorta3@gmail.com>
2026-01-17 14:56:35 +0100

    Avec nouvelle photo

--- projets/Jeu2d/obj/Debug/net8.0/Jeu2D.AssemblyInfo.cs
+++ projets/Jeu2d/obj/Debug/net8.0/Jeu2D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jeu2D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b6f0ef13bee86cab032e4d8f55e4170820b3ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7df89bfcd8ced81257a9b46aab20a793f14223")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jeu2D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jeu2D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
